1ca84d5d747adf7b4531514f24c23b190c498968
David Grier <david.grier@nyu.edu>
2017-06-13 07:33:38 -0400

    organizing for functionality

--- CGH.py
+++ CGH.py
@@ -25,26 +25,29 @@ class CGH(object):
     """
 
     def __init__(self, slm=None):
+
         self.slm = slm
+        # geometry in SLM plane
+        sx, sy = slm.center
         self.w = slm.width()
         self.h = slm.height()
-        self.m = QtGui.QMatrix4x4()
-        self.m.setToIdentity()
-        sx, sy = slm.center
-        self.rc = QtGui.QVector3D(320., 240., 0)
         factor = 2. * np.pi / self.w / 10.
         qx = np.linspace(0 - sx, self.w - 1 - sx, self.w)
         qy = np.linspace(0 - sy, self.h - 1 - sy, self.h)
         self.iqx = factor * 1j * qx
         self.iqy = factor * 1j * qy
+        # geometry in video plane
+        self.rc = QtGui.QVector3D(320., 240., 0)
+        self.m = QtGui.QMatrix4x4()
+        self.m.setToIdentity()
 
     @jit
     def setData(self, properties):
         psi = np.zeros((self.w, self.h), dtype=np.complex_)
         for property in properties:
             r = self.m * (property['r'] - self.rc)
-            fac = property['a'] * np.exp(1j * property['phi'])
-            psi += np.outer(fac * np.exp(self.iqy * r.y()),
+            amp = property['a'] * np.exp(1j * property['phi'])
+            psi += np.outer(amp * np.exp(self.iqy * r.y()),
                             np.exp(self.iqx * r.x()))
         phi = (256. * (np.angle(psi) / np.pi + 1.)).astype(np.uint8)
         self.slm.setData(phi)
