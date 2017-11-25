873d6b2d93964d323dc2ea969911a3cb6e6363b6
David Grier <david.grier@nyu.edu>
2017-11-25 18:28:47 -0500

    use frames for property sheets

--- QFabDVR.py
+++ QFabDVR.py
@@ -2,24 +2,27 @@ from PyQt4 import QtGui
 from fabdvr import fabdvr
 
 
-class QFabDVR(fabdvr, QtGui.QWidget):
+class QFabDVR(fabdvr, QtGui.QFrame):
 
     def __init__(self, **kwargs):
         super(QFabDVR, self).__init__(**kwargs)
         self.initUI()
 
     def initUI(self):
+        self.setFrameShape(QtGui.QFrame.Box)
         layout = QtGui.QGridLayout(self)
+        title = QtGui.QLabel('Video Recorder')
         self.brecord = QtGui.QPushButton('Record', self)
         self.bstop = QtGui.QPushButton('Stop', self)
         self.wframe = QtGui.QLCDNumber(self)
         self.wframe.setNumDigits(5)
         self.wfilename = QtGui.QLabel()
         self.wfilename.setText(self.filename)
-        layout.addWidget(self.brecord, 1, 1)
-        layout.addWidget(self.bstop, 1, 2)
-        layout.addWidget(self.wframe, 1, 3)
-        layout.addWidget(self.wfilename, 2, 1, 1, 3)
+        layout.addWidget(title, 1, 1, 1, 3)
+        layout.addWidget(self.brecord, 2, 1)
+        layout.addWidget(self.bstop, 2, 2)
+        layout.addWidget(self.wframe, 2, 3)
+        layout.addWidget(self.wfilename, 3, 1, 1, 3)
         self.setLayout(layout)
         self.brecord.clicked.connect(self.handleRecord)
         self.bstop.clicked.connect(self.handleStop)
@@ -34,11 +37,11 @@ class QFabDVR(fabdvr, QtGui.QWidget):
     def handleStop(self):
         super(QFabDVR, self).stop()
 
-        
+
 def main():
     import sys
     from QCameraItem import QCameraDevice, QCameraItem, QCameraWidget
-    
+
     app = QtGui.QApplication(sys.argv)
     device = QCameraDevice(size=(640, 480), gray=True)
     camera = QCameraItem(device)
@@ -47,7 +50,7 @@ def main():
     dvr = QFabDVR(camera=camera)
     dvr.show()
     sys.exit(app.exec_())
-    
+
 
 if __name__ == '__main__':
     main()
