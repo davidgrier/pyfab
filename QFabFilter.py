8828650231d73369dce1c513aca795b1fa55f68d
David Grier <david.grier@nyu.edu>
2017-11-28 14:02:55 -0500

    video filters

---
+++ QFabFilter.py
@@ -0,0 +1,49 @@
+from PyQt4 import QtGui, QtCore
+from vmedian import vmedian
+import numpy as np
+
+
+class QFabFilter(QtGui.QFrame):
+
+    def __init__(self, video):
+        super(QFabFilter, self).__init__()
+        self.video = video
+        self.init_filters()
+        self.init_ui()
+
+    def init_filters(self):
+        size = self.video.device.size
+        dim = (size.height(), size.width())
+        self.median = vmedian(order=3, dimensions=dim)
+
+    def init_ui(self):
+        self.setFrameShape(QtGui.QFrame.Box)
+        layout = QtGui.QVBoxLayout(self)
+        layout.setMargin(1)
+        title = QtGui.QLabel('Video Filters')
+        bmedian = QtGui.QCheckBox(QtCore.QString('Median'))
+        bnormalize = QtGui.QCheckBox(QtCore.QString('Normalize'))
+        layout.addWidget(title)
+        layout.addWidget(bmedian)
+        layout.addWidget(bnormalize)
+        bmedian.clicked.connect(self.handleMedian)
+        bnormalize.clicked.connect(self.handleNormalize)
+
+    def handleMedian(self, selected):
+        if selected:
+            self.video.registerFilter(self.median.filter)
+        else:
+            self.video.unregisterFilter(self.median.filter)
+
+    def normalize(self, frame):
+        med = np.clip(self.median.get(), 1, 255)
+        nrm = frame.astype(float) / med
+        self.median.add(frame)
+        return np.clip(100 * nrm, 0, 255).astype(np.uint8)
+                                     
+    def handleNormalize(self, selected):
+        if selected:
+            self.video.registerFilter(self.normalize)
+        else:
+            self.video.unregisterFilter(self.normalize)
+            
