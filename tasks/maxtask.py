991c0644aa1a6d8ceae75b9adbcf8779dcdc45d6
David Grier <david.grier@nyu.edu>
2017-12-15 13:58:22 -0500

    operate on maximum of several images

---
+++ tasks/maxtask.py
@@ -0,0 +1,25 @@
+from task import task
+import numpy as np
+
+
+class maxtask(task):
+
+    def __init__(self, nframes=5):
+        super(maxtask, self).__init__(self)
+        self.nframes = nframes
+        self.n = 0
+        self.frame = None
+
+    def process(self, frame):
+        if self.frame is None:
+            self.frame = frame
+        else:
+            self.frame = np.maximum(frame, self.frame)
+        self.n += 1
+        if self.n == self.nframes:
+            self.dotask()
+        self.done = True
+
+    def dotask(self):
+        print('task complete')
+        
