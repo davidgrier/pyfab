f157040d68d6a40b3a8f47c4221c1a69fd0c2363
David Grier <david.grier@nyu.edu>
2017-12-17 19:07:17 -0500

    implement menus

--- jansen.py
+++ jansen.py
@@ -4,26 +4,29 @@ import sys
 
 
 class jansen(QtGui.QMainWindow):
+
     def __init__(self):
         super(jansen, self).__init__()
         self.instrument = QJansenWidget()
         self.init_ui()
 
     def init_ui(self):
-        self.setWindowTitle('PyFab')
+        self.setWindowTitle('Jansen')
         self.statusBar().showMessage('Ready')
+
+        menubar = self.menuBar()
+        fileMenu = menubar.addMenu('&File')
+
         exitIcon = QtGui.QIcon.fromTheme('exit')
         exitAction = QtGui.QAction(exitIcon, '&Exit', self)
         exitAction.setShortcut('Ctrl-Q')
         exitAction.setStatusTip('Exit PyFab')
         exitAction.triggered.connect(QtGui.qApp.quit)
-
-        menubar = self.menuBar()
-        fileMenu = menubar.addMenu('&File')
         fileMenu.addAction(exitAction)
 
         self.setCentralWidget(self.instrument)
         self.show()
+
 
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
--- pyfab.py
+++ pyfab.py
@@ -2,9 +2,11 @@ from PyQt4 import QtGui
 from QFabWidget import QFabWidget
 from objects import fabconfig
 import sys
+import os
 
 
 class pyfab(QtGui.QMainWindow):
+
     def __init__(self):
         super(pyfab, self).__init__()
         self.init_ui()
@@ -17,19 +19,44 @@ class pyfab(QtGui.QMainWindow):
     def init_ui(self):
         self.setWindowTitle('PyFab')
         self.statusBar().showMessage('Ready')
-        exitIcon = QtGui.QIcon.fromTheme('exit')
+
+        menubar = self.menuBar()
+        menubar.setNativeMenuBar(False)
+        fileMenu = menubar.addMenu('&File')
+
+        snapIcon = QtGui.QIcon.fromTheme('camera-photo')
+        snapAction = QtGui.QAction(snapIcon, 'Save &Photo', self)
+        snapAction.setStatusTip('Save a snapshot')
+        snapAction.triggered.connect(self.savePhoto)
+        fileMenu.addAction(snapAction)
+
+        saveIcon = QtGui.QIcon.fromTheme('document-save')
+        saveAction = QtGui.QAction(saveIcon, '&Save Settings', self)
+        saveAction.setStatusTip('Save current settings')
+        saveAction.triggered.connect(self.saveSettings)
+        fileMenu.addAction(saveAction)
+
+        exitIcon = QtGui.QIcon.fromTheme('application-exit')
         exitAction = QtGui.QAction(exitIcon, '&Exit', self)
         exitAction.setShortcut('Ctrl-Q')
         exitAction.setStatusTip('Exit PyFab')
         exitAction.triggered.connect(QtGui.qApp.quit)
-
-        menubar = self.menuBar()
-        fileMenu = menubar.addMenu('&File')
         fileMenu.addAction(exitAction)
+
+    def savePhoto(self):
+        filename, filter = QtGui.QFileDialog.getSaveFileName(
+            parent=self, caption='Save Snapshot',
+            directory=os.path.expanduser('~/data'),
+            filter='*.png')
+        print(filename)
+
+    def saveSettings(self):
+        self.config.save(self.instrument.wcgh)
 
     def closeEvent(self, event):
         self.config.query_save(self.instrument.wcgh)
         self.instrument.close()
+
 
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
