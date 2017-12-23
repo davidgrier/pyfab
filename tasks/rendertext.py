3cecd42be76ee9c7e712a0036c5bf8aad0943da4
David Grier <david.grier@nyu.edu>
2017-12-23 18:22:22 -0500

    array indexing rather than flip

--- tasks/rendertext.py
+++ tasks/rendertext.py
@@ -19,10 +19,10 @@ class rendertext(task):
         img = Image.new('L', sz, 0)
         draw = ImageDraw.Draw(img)
         draw.text((0, 0), self.text, font=self.font, fill=255)
-        bmp = np.flip(np.array(img), 0) > 128
+        bmp = np.array(img) > 128
+        bmp = bmp[::-1]
         y, x = np.nonzero(bmp)
         x = (x - np.mean(x)) * 15 + 320
         y = (y - np.mean(y)) * 15 + 240
         p = list(map(lambda x, y: QVector3D(x, y, 0), x, y))
-        print('CREATING TRAPS ...')
         self.parent.pattern.createTraps(p)
