bda0ae4892d8d567c0b158c8106d3bde63b5db36
David Grier <david.grier@nyu.edu>
2017-12-26 11:18:05 -0500

    add clicked signal to widgets

---
+++ objects/clickable.py
@@ -0,0 +1,20 @@
+from PyQt4 import QtCore
+
+
+def clickable(widget):
+
+    class Filter(QtCore.QObject):
+
+        clicked = QtCore.pyqtSignal()
+
+        def eventFilter(self, obj, event):
+            if obj == widget:
+                if event.type() == QtCore.QEvent.MouseButtonRelease:
+                    if obj.rect().contains(event.pos()):
+                        self.clicked.emit()
+                        return True
+            return False
+
+    filter = Filter(widget)
+    widget.installEventFilter(filter)
+    return filter.clicked
