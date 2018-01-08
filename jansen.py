821a7c77427dc942f06fa2a5a2ed5e5761186ed6
David Grier <david.grier@nyu.edu>
2018-01-08 11:24:56 -0500

    make executables

--- jansen.py
+++ jansen.py
@@ -1,3 +1,8 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""Jansen is a video-capture GUI intended for holographic video microscopy"""
+
 from PyQt4 import QtGui
 from QJansenWidget import QJansenWidget
 import sys
--- pyfab.py
+++ pyfab.py
@@ -1,4 +1,7 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""pyfab is a GUI for holographic optical trapping"""
 
 from pyqtgraph.Qt import QtGui
 from QFabWidget import QFabWidget
