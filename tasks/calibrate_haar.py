cc043547096f0ad3794104ee43c658cd9174f932
David G. Grier <david.grier@nyu.edu>
2018-01-12 14:36:54 -0500

    progress on haar calibration

--- tasks/calibrate_haar.py
+++ tasks/calibrate_haar.py
@@ -2,6 +2,7 @@ from task import task
 from maxtask import maxtask
 from PyQt4.QtGui import QVector3D
 import numpy as np
+import matplotlib.pyplot as plt
 
 
 class haar_cleanup(task):
@@ -15,34 +16,40 @@ class haar_cleanup(task):
         self.done = True
 
     def addData(self, data):
+        print(data)
         self.data.append(data)
 
     def dotask(self):
-        print(self.data)
+        data = np.array(self.data)
+        plt.scatter(data[:,0], data[:,1])
+        plt.show()
+        print('done')
 
 
 class wavelet_response(maxtask):
 
-    def __init__(self, trap, cgh, slm, roi, summary,
-                 val=128., **kwargs):
+    def __init__(self, trap, cgh, slm, roi0, roi1, roi2,
+                 summary, val=128., **kwargs):
         super(wavelet_response, self).__init__(**kwargs)
         self.trap = trap
         self.cgh = cgh
         self.slm = slm
-        self.roi = roi
+        self.roi0 = roi0
+        self.roi1 = roi1
+        self.roi2 = roi2
         self.summary = summary
         self.val = val
 
     def initialize(self):
         psi = self.trap.psi
-        psi[0:psi.shape[0]/2, :] *= np.exp(1j * np.pi * self.val / 256.)
+        psi[0:psi.shape[0]/2,:] *= np.exp(1j * np.pi * self.val / 128.)
         self.slm.data = self.cgh.quantize(psi)
 
     def dotask(self):
-        roi = self.frame[self.roi]
-        inten = np.sum(roi)
-        self.summary.addData([self.val, inten])
-        print(self.val, inten)
+        v1 = np.sum(self.frame[self.roi1]).astype(float)
+        v0 = np.sum(self.frame[self.roi0]).astype(float)
+        bg = np.sum(self.frame[self.roi2]).astype(float)
+        self.summary.addData([self.val, (v1-bg)/(v0-bg)])
 
 
 class calibrate_haar(task):
@@ -52,20 +59,26 @@ class calibrate_haar(task):
 
     def initialize(self):
         self.parent.pattern.clearTraps()
+        cgh = self.parent.cgh
+        slm = self.parent.slm
+        dim = 15
+        xc = np.round(self.parent.cgh.rc.x()).astype(int)
+        yc = np.round(self.parent.cgh.rc.y()).astype(int)
+        roi0 = np.ogrid[yc-dim:yc+dim+1, xc-dim:xc+dim+1]
         xc = 100
         yc = 100
-        dim = 10
         pos = [QVector3D(xc, yc, 0)]
         self.parent.pattern.createTraps(pos)
         trap = self.parent.pattern.flatten()[0]
-        cgh = self.parent.cgh
-        slm = self.parent.slm
-        roi = np.ogrid[yc-dim:yc+dim+1, xc-dim:xc+dim+1]
+        roi1 = np.ogrid[yc-dim:yc+dim+1, xc-dim:xc+dim+1]
+        xc += 100
+        roi2 = np.ogrid[yc-dim:yc+dim+1, xc-dim:xc+dim+1]
         summary = haar_cleanup()
-        for val in range(0, 255, 10):
-            task = wavelet_response(trap, cgh, slm, roi,
-                                    summary,
-                                    val=val, nframes=5)
+        for val in range(0, 255, 2):
+            task = wavelet_response(trap, cgh, slm,
+                                    roi0, roi1, roi2,
+                                    summary, val=val,
+                                    delay=5, nframes=10)
             self.parent.tasks.registerTask(task)
         self.parent.tasks.registerTask(summary)
         self.done = True
