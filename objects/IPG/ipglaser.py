3f33e38d2800ff31145f195d661ecf28064554f5
David Grier <david.grier@nyu.edu>
2018-01-13 20:33:47 -0500

    testing for init failure

--- objects/IPG/ipglaser.py
+++ objects/IPG/ipglaser.py
@@ -14,12 +14,13 @@ class ipglaser(object):
         self.bytesize = serial.EIGHTBITS
         self.parity = serial.PARITY_NONE
         self.stopbits = serial.STOPBITS_ONE
-        self.findIPGLaser()
+        if not self.find():
+            raise ValueError('IPG Laser not found')
 
     def close(self):
         self.ser.close()
 
-    def findIPGLaser(self):
+    def find(self):
         ports = list(comports())
         if len(ports) <= 0:
             print('No serial ports found')
@@ -42,6 +43,7 @@ class ipglaser(object):
                                     fcntl.LOCK_EX | fcntl.LOCK_NB)
                     except IOError:
                         print 'Port {0} is busy'.format(self.ser.port)
+                        self.ser.close()
                         continue
             except serial.SerialException as ex:
                 print('Port {0} is unavailable: {1}'.format(port, ex))
@@ -50,15 +52,18 @@ class ipglaser(object):
             self.sio = io.TextIOWrapper(buffer, newline=self.eol,
                                         line_buffering=True)
             if self.identify():
-                return
+                return True
             self.ser.close()
         print('IPG Laser not found!')
+        return False
 
     def identify(self):
         res = self.command('RFV')
         return len(res) > 3
 
     def command(self, str):
+        if not self.isready:
+            return None
         cmd = unicode(str + self.eol)
         self.sio.write(cmd)
         res = self.sio.readline().decode().strip()
@@ -70,9 +75,10 @@ class ipglaser(object):
 
 
 def main():
-    a = ipglaser()
-    b = ipglaser()
+    a = ipglaser.find()
     print(a.power())
+
+    b = ipglaser()
     print(b.power())
 
 
