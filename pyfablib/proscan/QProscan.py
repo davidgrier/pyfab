97fc06191b5e0c1e6f80049747fa5d44a343f45b
= <michaelobrien@nyu.edu>
2018-02-02 11:46:54 -0500

    increase interval to address seg faults

--- pyfablib/proscan/QProscan.py
+++ pyfablib/proscan/QProscan.py
@@ -13,7 +13,7 @@ class QProscan(QtGui.QFrame):
 
         self._timer = QtCore.QTimer(self)
         self._timer.timeout.connect(self.update)
-        self._timer.setInterval(100)
+        self._timer.setInterval(200)
         self._timer.start()
 
     def stop(self):
