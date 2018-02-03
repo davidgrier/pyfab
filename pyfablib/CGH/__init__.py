12b1488658123dde19cf834a077d8be60db100bd
David Grier <david.grier@nyu.edu>
2018-02-03 09:54:53 -0500

    better layout

--- pyfablib/CGH/__init__.py
+++ pyfablib/CGH/__init__.py
@@ -3,9 +3,9 @@ try:
     from cudaCGH import cudaCGH as CGH
 except ImportError:
     logging.warning(
-        'Could not load CUDA CGH pipeline.\n\tFalling back to CPU pipeline.')
+        'Could not load CUDA CGH pipeline.\n' +
+        '\tFalling back to CPU pipeline.')
     from CGH import CGH
 from QCGH import QCGH
 
-__all__ = ['CGH',
-           'QCGH']
+__all__ = ['CGH', 'QCGH']
