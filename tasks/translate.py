556f4fc566f4d7543fd397503156fb3dbecc3e88
mjo22 <michaelobrien@nyu.edu>
2018-05-31 12:24:58 -0400

    documentation

--- tasks/translate.py
+++ tasks/translate.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-'''Translates traps a step in the z direction'''
+'''Translates traps in some fixed step and direction.'''
 
 from task import task
 from pyqtgraph.Qt import QtGui
@@ -9,12 +9,12 @@ from pyqtgraph.Qt import QtGui
 class translate(task):
 
     def __init__(self, traps=None, dr=QtGui.QVector3D(0, 0, 0), **kwargs):
-        super(translatez, self).__init__(**kwargs)
+        super(translate, self).__init__(**kwargs)
         self.traps = traps
-	self.dr = dr
+        self.dr = dr
 
-    def initialize(self, frame):
-        if self.traps is not None:
-            self.traps.select(state=True)
-            self.traps.moveBy(self.dr)
-            self.traps.select(state=False)
+        def initialize(self, frame):
+            if self.traps is not None:
+                self.traps.select(state=True)
+                self.traps.moveBy(self.dr)
+                self.traps.select(state=False)
