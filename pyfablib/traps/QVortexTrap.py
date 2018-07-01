06aa5d67f418ce524da03910368fd01fdbf18d41
David Grier <david.grier@nyu.edu>
2018-07-01 07:27:15 -0400

    custom symbol for vortex

--- pyfablib/traps/QVortexTrap.py
+++ pyfablib/traps/QVortexTrap.py
@@ -4,6 +4,8 @@
 
 from .QTrap import QTrap
 import numpy as np
+from pyqtgraph.Qt import QtGui
+
 import logging
 logging.basicConfig()
 logger = logging.getLogger(__name__)
@@ -13,12 +15,24 @@ class QVortexTrap(QTrap):
     def __init__(self, ell=10, **kwargs):
         super(QVortexTrap, self).__init__(**kwargs)
         self.ell = ell
+        self.spot['symbol'] = self.plotSymbol()
 
     def update_field(self):
         if self.cgh is None:
             logger.info('Tried to update without CGH')
             return
         self.structure = np.exp((1j * self.ell) * self.cgh.theta)
+
+    def plotSymbol(self):
+        sym = QtGui.QPainterPath()
+        sym.addText(0, 0, QtGui.QFont('San Serif', 10), 'V')
+        box = sym.boundingRect()
+        scale = min(1./box.width(), 1/box.height())
+        tr = QtGui.QTransform()
+        tr.scale(scale, scale)
+        tr.translate(-box.x() - box.width()/2., -box.y() - box.height()/2.)
+        sym = tr.map(sym)
+        return sym
 
     @property
     def cgh(self):
