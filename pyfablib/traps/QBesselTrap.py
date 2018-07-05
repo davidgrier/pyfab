2af86de31153a7a3c25b8045fb0597fda2a4e055
ninawittler <wittler.nina@gmail.com>
2018-07-05 10:36:18 -0400

    Adding QBesselTrap

---
+++ pyfablib/traps/QBesselTrap.py
@@ -0,0 +1,25 @@
+# coding: utf-8
+
+from QTrap import QTrap
+import numpy as np
+import cmath 
+from pyqtgraph.Qt import QtGui
+
+
+
+class QBesselTrap(QTrap):
+    def __init__(self, **kwargs):
+        super(QBesselTrap, self).__init__()
+        self.shift = 100
+        self.shiftx = 0
+        self.shifty = 0
+        self.modetot = 1
+    
+    def update_structure(self):
+        xv, yv = np.meshgrid(self.qx, self.qy)
+        xv, yv = xv.T, yv.T
+        qr = np.hypot.outer(self.qx, self.qy)
+        phi = np.remainder(np.angle(self.modetot) - self.shift*qr -
+                           self.shiftx*xv - self.shifty*yv, 2*(np.pi))
+        self.structure = np.exp((1j) * self.cgh.phi)
+     
