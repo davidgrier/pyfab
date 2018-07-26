84523b9631238ae12075eced6fdb938faaa91378
ninawittler <wittler.nina@gmail.com>
2018-07-26 13:07:09 -0400

    simplifying QBesselTrap

--- pyfablib/traps/QBesselTrap.py
+++ pyfablib/traps/QBesselTrap.py
@@ -1,13 +1,10 @@
-# coding: utf-8
+# -*- coding: utf-8 -*-
+
+"""QBesselTrap.py: Bessel Trap"""
 
 from .QTrap import QTrap
 import numpy as np
-import cmath 
-from pyqtgraph.Qt import QtCore, QtGui
-
-import logging
-logging.basicConfig()
-logger = logging.getLogger(__name__)
+from pyqtgraph.Qt import QtGui
 
 class QBesselTrap(QTrap):
     def __init__(self, **kwargs):
@@ -16,18 +13,18 @@ class QBesselTrap(QTrap):
 
     def update_structure(self):
         phi = np.remainder(np.angle(1) - 100*self.cgh.qr, 2*(np.pi))
-        self.structure = np.exp((1j) * self.cgh.phi)
-        self._update()
+        self.structure = np.exp(1j * phi)
+        self._update() 
         
     def plotSymbol(self):
         sym = QtGui.QPainterPath()
         font = QtGui.QFont('Sans Serif', 10, QtGui.QFont.Black)
         sym.addText(0, 0, font, 'b')
-        # Scale symbol to unit square
+        # scale symbol to unit square
         box = sym.boundingRect()
         scale = 1./max(box.width(), box.height())
         tr = QtGui.QTransform().scale(scale, scale)
-        # Center symbol on (0, 0)
+        # center symbol on (0, 0)
         tr.translate(-box.x() - box.width()/2., -box.y() - box.height()/2.)
         return tr.map(sym)
     
