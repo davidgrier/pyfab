e39c0254206315bb0b3f3a0ad0b1cf0b629d5ea4
ninawittler <wittler.nina@gmail.com>
2018-07-31 12:16:04 -0400

    Bessel trap's shift property is now adjustable in 'Traps' tab of pyfab

--- pyfablib/traps/QBesselTrap.py
+++ pyfablib/traps/QBesselTrap.py
@@ -8,10 +8,11 @@ from pyqtgraph.Qt import QtGui
 
 
 class QBesselTrap(QTrap):
-    def __init__(self, shift=1000, **kwargs):
+    def __init__(self, shift=100, **kwargs):
         super(QBesselTrap, self).__init__(**kwargs)
         self._shift = shift
-
+        self.registerProperty('shift', tooltip=True)
+        
     def updateStructure(self):
         phi = np.remainder(np.angle(1) - self.shift*self.cgh.qr, 2*(np.pi))
         self.structure = np.exp(1j * phi)
@@ -36,5 +37,5 @@ class QBesselTrap(QTrap):
     @shift.setter
     def shift(self, shift):
         self._shift = np.int(shift)
-        self.update_structure()
+        self.updateStructure()
         self.valueChanged.emit(self)
