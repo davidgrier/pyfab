c97c0439a5201347743e3fc6faada82bfdac0965
Grier Group <group@boltzmania.physics.nyu.edu>
2019-01-04 16:08:48 -0500

    recording background is option

--- tasks/moverecordz.py
+++ tasks/moverecordz.py
@@ -10,9 +10,10 @@ import os
 class moverecordz(task):
     """Delay, record, and translate traps in the z direction."""
 
-    def __init__(self, **kwargs):
+    def __init__(self, measure_bg=False, **kwargs):
         super(moverecordz, self).__init__(**kwargs)
         self.traps = None
+        self.measure_bg = False
 
     def initialize(self, frame):
         self.traps = self.parent.pattern.pattern
@@ -29,15 +30,18 @@ class moverecordz(task):
             z = self.traps.r.z()
             dz = -10
             dr = QtGui.QVector3D(0, 0, dz)
-            for n in range(0, 50):
+            for n in range(0, 30):
                 z_nom = np.absolute(z + dz*n)
-                self.register('movetopoint', x=self.r_bg[0], y=self.r_bg[1], z=None)
-                self.register('delay', delay=30)
-                self.register('record', fn=fn0+'_bg_{:03d}.avi'.
-                              format(int(z_nom)))
-                self.register('movetopoint', x=self.r[0], y=self.r[1], z=None)
+                if self.measure_bg:
+                    self.register('movetopoint',
+                                  x=self.r_bg[0], y=self.r_bg[1], z=None)
+                    self.register('delay', delay=50)
+                    self.register('record', fn=fn0+'_bg_{:03d}.avi'.
+                                  format(int(z_nom)), nframes=50)
+                    self.register('movetopoint',
+                                  x=self.r[0], y=self.r[1], z=None)
                 self.register('delay', delay=60)
                 self.register('record', fn=fn0+'{:03d}.avi'.
                               format(int(z_nom)),
-                              nframes=100)
+                              nframes=50)
                 self.register('translate', traps=self.traps, dr=dr)
