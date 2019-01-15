7c8cfc480aa7383555ebccaf6ff5359a3834341c
Grier Group <group@boltzmania.physics.nyu.edu>
2019-01-15 13:06:21 -0500

    movetopoint generalized to movetocoordinate. by default it moves all traps to focal plane

--- tasks/moverecordz.py
+++ tasks/moverecordz.py
@@ -25,23 +25,20 @@ class moverecordz(task):
 
     def dotask(self):
         self.traps = self.parent.pattern.pattern
-        r_i = {}
-        for trap in self.traps.flatten():
-            r_i[trap] = (trap.r.x(), trap.r.y(), trap.r.z())
         if self.traps.count() > 0:
             fn0, fn_ext = os.path.splitext(self.parent.dvr.filename)
             z = self.traps.r.z()
             dz = -10
             dr = QtGui.QVector3D(0, 0, dz)
-            for n in range(0, 25):
+            for n in range(0, 2):
                 z_nom = np.absolute(z + dz*n)
                 if self.measure_bg:
-                    self.register('movetopoint',
+                    self.register('movetocoordinate',
                                   x=self.r_bg[0], y=self.r_bg[1], z=None)
                     self.register('delay', delay=50)
                     self.register('record', fn=fn0+'bg_{:03d}.avi'.
                                   format(int(z_nom)), nframes=50)
-                    self.register('movetopoint',
+                    self.register('movetocoordinate',
                                   x=self.r[0], y=self.r[1], z=None)
                 self.register('delay', delay=50)
                 self.register('record', fn=fn0+'{:03d}.avi'.
@@ -49,5 +46,4 @@ class moverecordz(task):
                               nframes=50)
                 self.register('delay', delay=10)
                 self.register('translate', traps=self.traps, dr=dr)
-            self.register('guidedmove', targets=r_i)
-            self.register('correct', positions=r_i)
+            self.register('movetocoordinate')
---
+++ tasks/movetocoordinate.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+# MENU: Motion/Move to focal plane
+
+from .guidedmove import guidedmove
+import numpy as np
+
+
+class movetocoordinate(guidedmove):
+    """Can either align many traps in the same line or plane or
+    can move one trap to a fixed point.
+    By default this task brings all traps to the focal plane."""
+
+    def __init__(self, x=None, y=None, z=0, **kwargs):
+        super(movetocoordinate, self).__init__(**kwargs)
+        self.x = x
+        self.y = y
+        self.z = z
+
+    def calculate_targets(self, traps):
+        targets = {}
+        for trap in traps.flatten():
+            x, y, z = (self.x, self.y, self.z)
+            if x is None:
+                x = trap.r.x()
+            if y is None:
+                y = trap.r.y()
+            if z is None:
+                z = trap.r.z()
+            targets[trap] = np.array((x, y, z))
+        self.register('correct', positions=targets)
+        return targets
