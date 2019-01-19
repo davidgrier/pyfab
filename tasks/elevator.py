6f3b39f8b2d7a53fdcff2e6f9cc770bff12961c2
mjo22 <michaelobrien@nyu.edu>
2019-01-18 21:20:10 -0500

    task to record video and save machine z value while moving a particle in z

--- tasks/elevator.py
+++ tasks/elevator.py
@@ -1,15 +1,34 @@
 # -*- coding: utf-8 -*-
 # MENU: Experiments/Measure z continuous
 
-from .task import task
+from .movetocoordinate import movetocoordinate
+import pandas as pd
 
 
-class elevator(task):
+class elevator(movetocoordinate):
     """Automatically move traps up then down in the z direction."""
 
     def __init__(self, **kwargs):
-        super(elevator, self).__init__(**kwargs)
+        super(elevator, self).__init__(z=-200,
+                                       travel_back=True,
+                                       correct=False,
+                                       **kwargs)
 
-    def initialize(self, frame):
-        self.register('movetocoordinate', z=200, correct=False)
-        self.register('movetocoordinate')
+    def initialize_more(self, frame):
+        self.dvr = self.parent.dvr
+        self.dvr.brecord.animateClick()
+        self.trap_list = self.parent.pattern.pattern.flatten()
+        self.frames = []
+        self.data = {}
+        for trap in self.trap_list:
+            self.data[trap] = []
+
+    def process_more(self, frame):
+        self.frames.append(self.dvr.framenumber)
+        for trap in self.trap_list:
+            self.data[trap].append(trap.r.z())
+
+    def dotask(self):
+        self.dvr.bstop.animateClick()
+        df = pd.DataFrame(index=self.frames, data=self.data)
+        df.to_csv(self.dvr.filename[:-4] + ".csv")
