c82d46d1e84bc4648b15d3e1ed2a6ea9359f46fa
David Grier <david.grier@nyu.edu>
2019-02-07 14:46:45 -0500

    glue class for Designer UI and instruments

---
+++ common/QSettingsWidget.py
@@ -0,0 +1,252 @@
+# -*- coding: utf-8 -*-
+
+from PyQt5.QtCore import pyqtSlot
+from PyQt5.QtWidgets import (
+    QWidget, QComboBox, QSpinBox, QDoubleSpinBox, QPushButton)
+import inspect
+
+import logging
+logging.basicConfig()
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
+
+class QSettingsWidget(QWidget):
+    '''A glue class that connects a device with a GUI
+
+    An object of this class associates properties of a device
+    such as a camera with widgets in a GUI that are
+    intended to control those properties.  Getting and setting
+    the properties with this class ensures that both the device
+    and the GUI are up to date and synchronized.
+
+    ...
+
+    Parameters
+    ----------
+    device : object
+        Properties of this device will be selected
+    ui : object
+        UI object created with QtDesigner
+    parent : QWidget
+        Parent widget in PyQt hierarchy
+
+    Attributes
+    ----------
+    properties : list
+        List of properties that are managed by the object
+    settings : dict
+        Dictionary of properties and their settings
+
+    Methods
+    -------
+    set(property, value)
+        Set the named property to the specified value.
+        This updates both the UI and the device.
+    get(property) : int|float|bool
+        Get the named property
+    '''
+
+    def __init__(self, parent=None, device=None, ui=None):
+        '''
+        Parameters
+        ----------
+        parent : QWidget
+            Parent widget in PyQt hierarchy
+        device : object
+            Abstraction of device to be controlled
+        ui : object
+            GUI representing device created by QtDesigner
+        '''
+        super(QSettingsWidget, self).__init__(parent)
+        self.ui = ui
+        self.ui.setupUi(self)
+        self._properties = []
+        self.device = device
+
+    @property
+    def device(self):
+        '''Object representation of device to be controlled'''
+        return self._device
+
+    @device.setter
+    def device(self, device):
+        if device is None:
+            self.setEnabled(False)
+            self._device = None
+            return
+        if hasattr(self, 'device'):
+            self.disconnectSignals()
+        self._device = device
+        self.getProperties()
+        self.configureUi()
+        self.updateUi()
+        self.connectSignals()
+        self.setEnabled(True)
+        logger.info('device connected')
+
+    def set(self, name, value):
+        '''Set parameter on both device and UI
+
+        Parameters
+        ----------
+        name : str
+            Name of property
+        value : scalar
+            Value of property
+        '''
+        if name in self.properties:
+            self.setDeviceProperty(name, value)
+            self.setUiProperty(name, value)
+        else:
+            logger.warning('unknown property: {}'.format(name))
+
+    def get(self, name):
+        '''Return value from device (and UI)
+
+        Parameters
+        ----------
+        name : str
+            Name of property
+
+        Returns
+        -------
+        value : scalar
+            Value of property
+        '''
+        if name in self.properties:
+            return getattr(self.device, name)
+        else:
+            logger.warning('unknown property: {}'.format(name))
+
+    def setDeviceProperty(self, name, value):
+        '''Set device property and wait for operation to complete
+
+        Parameters
+        ----------
+        name : str
+            Name of property to set
+        value : scalar
+            Value to set
+        '''
+        if hasattr(self.device, name):
+            setattr(self.device, name, value)
+            logger.info('Setting {}: {}'.format(name, value))
+            while self.device.busy():
+                if self.device.error:
+                    logger.warn('device error')
+
+    def setUiProperty(self, name, value):
+        '''Set UI property
+
+        Parameters
+        ----------
+        name : str
+            Name of property to set
+        value : scalar
+            Value to set
+        '''
+        wid = getattr(self.ui, name)
+        if isinstance(wid, QDoubleSpinBox):
+            wid.setValue(value)
+        elif isinstance(wid, QSpinBox):
+            wid.setValue(value)
+        elif isinstance(wid, QComboBox):
+            wid.setCurrentIndex(value)
+        elif isinstance(wid, QPushButton):
+            pass
+        else:
+            logger.warn('Unknown property: {}: {}'.format(name, type(wid)))
+
+    @property
+    def settings(self):
+        '''Dictionary of properties and their values'''
+        values = dict()
+        for prop in self.properties:
+            value = getattr(self.device, prop)
+            if not inspect.ismethod(value):
+                values[prop] = value
+        return values
+
+    @settings.setter
+    def settings(self, values):
+        for name in values:
+            self.setDeviceProperty(name, values[name])
+        self.updateUi()
+
+    @property
+    def properties(self):
+        '''List of properties managed by this object'''
+        return self._properties
+
+    def getProperties(self):
+        '''Create list of properties
+
+        Valid properties appear in both the device and the ui.
+        Do not include private properties denoted with an underscore.
+        '''
+        dprops = [name for name, _ in inspect.getmembers(self.device)]
+        uprops = [name for name, _ in inspect.getmembers(self.ui)]
+        props = [name for name in dprops if name in uprops]
+        self._properties = [name for name in props if '_' not in name]
+        logger.debug(self._properties)
+
+    def configureUi(self):
+        logger.debug('configureUi should be overridden')
+
+    @pyqtSlot()
+    def updateUi(self):
+        '''Update widgets with current values from device'''
+        for prop in self.properties:
+            val = getattr(self.device, prop)
+            self.setUiProperty(prop, val)
+
+    @pyqtSlot(int)
+    @pyqtSlot(float)
+    def updateDevice(self, value):
+        '''Update device property when UI property is updated
+
+        Connecting this slot to the appropriate UI signal ensures
+        that device properties are updated when the user interacts
+        with the UI.
+        '''
+        name = str(self.sender().objectName())
+        self.setDeviceProperty(name, value)
+
+    @pyqtSlot(bool)
+    def autoUpdateDevice(self, flag):
+        autosetproperty = self.sender.objectName()
+        autosetmethod = getattr(self.device, autosetproperty)
+        autosetmethod()
+        while self.device.busy():
+            if self.device.error:
+                logger.warn('device error')
+        self.updateUi
+
+    def connectSignals(self):
+        for prop in self.properties:
+            wid = getattr(self.ui, prop)
+            if isinstance(wid, QDoubleSpinBox):
+                wid.valueChanged.connect(self.updateDevice)
+            elif isinstance(wid, QSpinBox):
+                wid.valueChanged.connect(self.updateDevice)
+            elif isinstance(wid, QComboBox):
+                wid.currentIndexChanged.connect(self.updateDevice)
+            elif isinstance(wid, QPushButton):
+                wid.clicked.connect(self.autoUpdateDevice)
+            else:
+                logger.warn('Unknown property: {}: {}'.format(prop, type(wid)))
+
+    def disconnectSignals(self):
+        for prop in self.properties:
+            wid = getattr(self.ui, prop)
+            if isinstance(wid, QDoubleSpinBox):
+                wid.valueChanged.disconnect(self.updateDevice)
+            elif isinstance(wid, QSpinBox):
+                wid.valueChanged.disconnect(self.updateDevice)
+            elif isinstance(wid, QComboBox):
+                wid.currentIndexChanged.disconnect(self.updateDevice)
+            elif isinstance(wid, QPushButton):
+                wid.clicked.disconnect(self.autoUpdateDevice)
+            else:
+                logger.warn('Unknown property: {}: {}'.format(prop, type(wid)))
