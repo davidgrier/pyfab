fdca16cdd59a39f28b475ef3ddce32c46161adb5
mjo22 <michaelobrien@nyu.edu>
2020-01-11 14:46:21 -0500

    Significant speedups with scipy parallelized integration and numba function evaluations.

--- pyfablib/traps/QCustomTrap.py
+++ pyfablib/traps/QCustomTrap.py
@@ -5,17 +5,27 @@ QCustomTrap.py: Drawing a trap along a parametric curve.
 
 REFERENCES:
 1. José A. Rodrigo, Tatiana Alieva, Eugeny Abramochkin, and Izan Castro   , "Shaping of light beams along curves in three dimensions," Opt.
-   Express 21, 20544-20555 (2013) 
+   Express 21, 20544-20555 (2013)
 
 2. José A. Rodrigo and Tatiana Alieva, "Freestyle 3D laser traps: tools   for studying light-driven particle dynamics and beyond," Optica 2,
-   812-815 (2015) 
+   812-815 (2015)
 """
 
 from .QTrap import QTrap
 import numpy as np
-from scipy import interpolate
+from scipy import integrate
+from numba import njit
 from pyqtgraph.Qt import QtGui
 from time import time
+
+
+@njit(parallel=True)
+def integrand(x, y, x_0, y_0, z_0, dr_0, S_t, S_T, rho, m, f, lamb):
+    Phi = np.exp(1.j * (y * x_0 - x * y_0) / rho**2
+                 + 1.j * 2*np.pi * m * S_t / S_T)
+    phi = np.exp(1.j*np.pi * z_0 * ((x - x_0)**2 + (y - y_0)**2)
+                 / (lamb * f**2))
+    return Phi*phi*dr_0
 
 
 class QCustomTrap(QTrap):
@@ -28,35 +38,29 @@ class QCustomTrap(QTrap):
         self.registerProperty('m', decimals=0, tooltip=True)
 
     def updateStructure(self):
-        start = time()
-        # Allocate geometrical buffers
-        phi = np.zeros(self.cgh.shape, dtype=np.complex64)
-        alpha = np.cos(np.radians(self.cgh.phis))
-        x = alpha*(np.arange(self.cgh.width) - self.cgh.xs)
-        y = np.arange(self.cgh.height) - self.cgh.ys
+        t0 = time()
         # Allocate integration range
         self.T = 2 * np.pi
         t = np.linspace(0, self.T, 100, endpoint=True)
+        # Allocate geometrical buffers
+        integrand = np.zeros((t.size,
+                              self.cgh.shape[0],
+                              self.cgh.shape[1]),
+                             dtype=np.complex_)
+        x = self.cgh.qr * np.cos(self.cgh.theta) / self.cgh.qprp
+        y = self.cgh.qr * np.sin(self.cgh.theta) / self.cgh.qprp
         # Compute integrals for normalization
-        self.S_T = self.S(self.T)
+        S_T = self.S(self.T)
         dr_0 = self.dr_0(t)
-        params = interpolate.splrep(t, dr_0, s=0)
-        spline = interpolate.BSpline(*params)
-        L = spline.integrate(0, self.T)
-        # Integrate at each pixel
-        for j, yj in enumerate(y):
-            for i, xi in enumerate(x):
-                integrand = self.integrand(t, xi, yj)
-                real_params = interpolate.splrep(t, integrand.real, s=0)
-                imag_params = interpolate.splrep(t, integrand.imag, s=0)
-                real_spline = interpolate.BSpline(*real_params)
-                imag_spline = interpolate.BSpline(*imag_params)
-                real = real_spline.integrate(0, self.T)
-                imag = imag_spline.integrate(0, self.T)
-                phi[j, i] = (real+imag*1.j) / L
-                print(j, i)
-        print("Time to compute: {}".format(time() - start))
-        self.structure = phi
+        L = integrate.simps(dr_0, x=t)
+        # Evaluate integrand at all points along the curve
+        f = self.cgh.focalLength
+        lamb = self.cgh.wavelength
+        for idx, ti in enumerate(t):
+            integrand[idx] = self.integrand(ti, x, y, S_T, f, lamb)
+        # Integrate
+        self.structure = integrate.simps(integrand, x=t, axis=0) / L
+        print("Time to compute: {}".format(time() - t0))
 
     def plotSymbol(self):
         sym = QtGui.QPainterPath()
@@ -93,17 +97,13 @@ class QCustomTrap(QTrap):
         self.updateStructure()
         self.valueChanged.emit(self)
 
-    def integrand(self, t, x, y):
+    def integrand(self, t, x, y, S_T, f, lamb):
         '''Integrand for Eq. (6) in Rodrigo (2015)'''
-        focalLength = self.cgh.focalLength
-        wavelength = self.cgh.wavelength
-        phi = np.exp(1.j * np.pi * self.z_0(t) *
-                     ((x - self.x_0(t))**2 + (y - self.y_0(t))**2) /
-                     (wavelength * focalLength**2))
-        Phi = np.exp(1.j * (y * self.x_0(t) - x * self.y_0(t)) / self.rho**2
-                     + 1.j * 2*np.pi * self.m * self.S(t) / self.S_T)
+        x_0, y_0, z_0 = (self.x_0(t), self.y_0(t), self.z_0(t))
         dr_0 = self.dr_0(t)
-        return Phi*phi*dr_0
+        S_t = self.S(t)
+        return integrand(x, y, x_0, y_0, z_0, dr_0, S_t, S_T,
+                         self.rho, self.m, f, lamb)
 
     def S(self, T):
         '''
