f8611dfbcba8118603726c6ae23d87e2409e5aba
mjo22 <michaelobrien@nyu.edu>
2020-03-20 17:02:27 -0400

    Subclass new Assemble task. The last QTrapGroup created by user is used for assembly.

--- tasks/Circle.py
+++ tasks/Circle.py
@@ -1,39 +1,31 @@
 # -*- coding: utf-8 -*-
 # MENU: Motion/Assemble circle
 
-from .Task import Task
+from .Assemble import Assemble
 import numpy as np
 
 
-class Circle(Task):
+class Circle(Assemble):
     """Demonstration of traps assembling a circle."""
 
     def __init__(self, **kwargs):
         super(Circle, self).__init__(**kwargs)
 
     def dotask(self):
-        cgh = self.parent.cgh.device
-        pattern = self.parent.pattern.pattern
-        group = None
-        for child in reversed(pattern.children()):
-            if isinstance(child, type(pattern)):
-                group = child
-                break
-        assembler = self.parent.assembler
-        # Set traps from last QTrapGroup
-        assembler.traps = group
-        # Calculate vertices of circle
-        vertices = []
-        radius = 200  # pixels
-        xc, yc = (cgh.xc, cgh.yc)
-        traps = assembler.traps.flatten()
-        ntraps = len(traps)
-        zrange = np.arange(0, 100, 2)
-        for idx, trap in enumerate(traps):
-            theta = 2*np.pi*(idx+1) / ntraps
-            z = np.random.choice(zrange)
-            vertices.append(np.array([xc + radius*np.cos(theta),
-                                      yc + radius*np.sin(theta),
-                                      z]))
-        assembler.targets = vertices
-        assembler.start()
+        if self.assembler.traps is not None:
+            # Calculate vertices of circle
+            vertices = []
+            radius = 200  # pixels
+            xc, yc = (self.cgh.xc, self.cgh.yc)
+            traps = self.assembler.traps.flatten()
+            ntraps = len(traps)
+            zrange = np.arange(0, 100, 2)
+            for idx, trap in enumerate(traps):
+                theta = 2*np.pi*(idx+1) / ntraps
+                z = np.random.choice(zrange)
+                vertices.append(np.array([xc + radius*np.cos(theta),
+                                          yc + radius*np.sin(theta),
+                                          z]))
+            # Set vertices and begin assembly
+            self.assembler.targets = vertices
+            self.assembler.start()
