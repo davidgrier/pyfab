0816e4a0e718fc895d4bd9e79916391cd1e9c580
David Grier <david.grier@nyu.edu>
2020-06-21 07:04:58 -0400

    create ring traps

---
+++ tasks/lib/RingTrap.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+# MENU: Add trap/Ring trap
+
+from ..QTask import QTask
+from pyfablib.traps import QRingTrap
+from PyQt5.QtGui import QVector3D
+
+
+class RingTrap(QTask):
+    """Add a ring trap to the trapping pattern"""
+
+    def __init__(self, **kwargs):
+        super(RingTrap, self).__init__(**kwargs)
+
+    def dotask(self):
+        cgh = self.parent().cgh.device
+        trap = QRingTrap(r=QVector3D(cgh.xc, cgh.yc, 0))
+        self.parent().pattern.addTrap(trap)
