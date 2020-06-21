a9a7cf1e8db00764727216a6515b701cfc851ea5
David Grier <david.grier@nyu.edu>
2020-06-21 07:20:14 -0400

    create trap at optical axis

--- tasks/lib/VortexTrap.py
+++ tasks/lib/VortexTrap.py
@@ -3,7 +3,7 @@
 
 from ..QTask import QTask
 from pyfablib.traps import QVortexTrap
-from pyqtgraph.Qt import QtGui
+from PyQt5.QtGui import QVector3D
 
 
 class VortexTrap(QTask):
@@ -13,5 +13,7 @@ class VortexTrap(QTask):
         super(VortexTrap, self).__init__(**kwargs)
 
     def complete(self):
-        trap = QVortexTrap(r=QtGui.QVector3D(100, 100, 0))
+        cgh = self.parent().cgh.device
+        pos = QVector3D(cgh.xc, cgh.yc, 0.)
+        trap = QVortexTrap(r=pos)
         self.parent().pattern.addTrap(trap)
