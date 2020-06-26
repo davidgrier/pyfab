067a5345080d9d1693842c1622fa9b361974868a
David Grier <david.grier@nyu.edu>
2020-06-25 20:52:48 -0400

    remove extraneous condition

--- tasks/lib/Record.py
+++ tasks/lib/Record.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
+# MENU: DVR/Record
 
-'''Task to record a video for a certain number of frames.'''
+'''Task to record a specified number of frames.'''
 
 from ..QTask import QTask
 
@@ -8,7 +9,6 @@ from ..QTask import QTask
 class Record(QTask):
 
     def __init__(self, fn=None, nframes=30, **kwargs):
-        # Pass in nframes keyword for length of recording
         super(Record, self).__init__(nframes=nframes, **kwargs)
         self.fn = fn
 
@@ -19,5 +19,4 @@ class Record(QTask):
         self.dvr.recordButton.animateClick()
 
     def complete(self):
-        if self.stop:
-            self.dvr.stopButton.animateClick()
+        self.dvr.stopButton.animateClick()
