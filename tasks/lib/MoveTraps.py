aade6ac2f7353043f71543b675953f48aca590e5
Jackie Sustiel <43007813+sustielz@users.noreply.github.com>
2020-07-07 16:57:35 -0400

    Simplify moveTraps
    Change trajectory to a list of trajectories, for clarity: i.e. each trap has one trajectory. Also simplify 'process'

--- tasks/lib/MoveTraps.py
+++ tasks/lib/MoveTraps.py
@@ -6,24 +6,18 @@ from ..QTask import QTask
 class MoveTraps(QTask):
     '''Move specified traps along specified trajectory'''
 
-    def __init__(self, traps=None, trajectory=None, **kwargs):
+    def __init__(self, traps=None, trajectories=None, **kwargs):
         super(MoveTraps, self).__init__(**kwargs)
-        self.traps = traps
-        self.trajectory = trajectory
-        self.nframes = len(self.trajectory) * self.skip
+        self.traps = traps if isinstance(traps, list) else [traps]
+        self.trajectories = trajectories if isinstance(trajectories, list) else [trajectories]
+        self.nframes = len(self.trajectories[0]) * self.skip
         self.pattern = self.parent().pattern.pattern
-        if isinstance(self.traps, list):
-            self.process = self.processTraps
-        else:
-            self.process = self.processTrap
 
-    def processTrap(self, frame):
-        pos = self.trajectory.pop(0)
-        self.traps.moveTo(pos)
-
-    def processTraps(self, frame):
-        positions = self.trajectory.pop(0)
+    def process(self, frame):
+        positions = [traj.pop(0) for traj in self.trajectories]
         self.pattern.blockRefresh(True)
         map(lambda trap, pos: trap.moveTo(pos), self.traps, positions)
         self.pattern.blockRefresh(False)
         self.pattern.refresh()
+
+        
