9b9feb603f74104f81527bf7fc5f427b34e0bf42
Jackie Sustiel <43007813+sustielz@users.noreply.github.com>
2020-07-09 21:58:15 -0400

    Update Vision.py

--- tasks/lib/Vision.py
+++ tasks/lib/Vision.py
@@ -1,13 +1,21 @@
 # -*- coding: utf-8 -*-
 
 from PyQt5.QtCore import pyqtSlot, pyqtSignal, pyqtProperty, QThread, QObject
-from ..QTask import QTask
+from .QTask import QTask
 
 from pylorenzmie.analysis import Video, Frame
+from pylorenzmie.theory import Instrument
+
+import CNNLorenzMie as cnn
 
 import numpy as np
-import pyqtgraph as pg
+#import pyqtgraph as pg
 import ujson as json
+# from scipy.stats import gaussian_kde
+import matplotlib.cm as cm
+import os
+# import json
+# from time import time
 
 import logging
 logging.basicConfig()
@@ -15,18 +23,12 @@ logger = logging.getLogger(__name__)
 logger.setLevel(logging.INFO)
 # logger.setLevel(logging.DEBUG)
 
-from pylorenzmie.analysis import Frame
-from pylorenzmie.theory import Instrument
-
-from scipy.stats import gaussian_kde
-
-import CNNLorenzMie as cnn
-import numpy as np
-import pyqtgraph as pg
-import matplotlib.cm as cm
-import os
-import json
-from time import time
+class settings(Enum):
+    raw = 0
+    detect = 1
+    estimate = 2
+    refine = 3
+    
 class Vision(QTask):
     
 #     sigDone = pyqtSignal(self)    ## Should tasks always return themselves? Maybe add this to QTask.py
@@ -35,22 +37,24 @@ class Vision(QTask):
 
     def __init__(self, **kwargs):
         super(Vision, self).__init__(blocking=False, **kwargs)
-       
-        
-    def initialize(self, frame):
         """Perform initialization operations"""
         logger.debug('Initializing')
          # Initialize serialized properties
         self._linkTol = 20.
         self._confidence = 50.
         self._maxSize = 603
-        self._realTime = True
+#         self._realTime = True
 #         self._postProcess = False
+#         self._saveFrames = False
+#         self._saveTrajectories = False
+#         self._saveFeatureData = False
+
 
         # Set non-serialized properties
-        self.detect = False
-        self.estimate = False
-        self.refine = False
+        self.setting = settings.raw
+        self.realtime = settings.detect
+        self.post = settings.estimate
+        self.link = settings.detect
         self.localizer = None
         self.estimator = None
 
@@ -61,7 +65,45 @@ class Vision(QTask):
         self.filename = None
         self.frames = []
         self.framenumbers = []
-                    
+        
+        self.count = 0
+        self.runTime = self.nframes
+    
+    
+        
+    #
+    # Slots
+    #
+    def resetVideo():
+        self.video._frames = []
+        self.video._trajectories = []
+        self.count = 0
+        
+    def initialize(self, frame):
+        self.resetVideo()
+        
+    def process(self, frame):
+        if self.count is self.runTime:
+            self.setting = self.realtime
+            self.video.frames = self.predict(self.video.frames, post=True)
+        elif self.count < self.runTime:
+            self.video.add([ self.predict([frame]) ])
+        self.count += 1
+#         self.remove()
+#         if self.counter == 0:
+#             self.counter = self._nskip
+#             i = self.jansen.dvr.framenumber
+#             if self.realTime:
+#                 frames, detections = self.predict([image], [i])
+#                 frame = frames[0]
+#                 self.sigNewFrame.emit(frame)
+
+#                 if self.jansen.dvr.is_recording():
+#                     self.recording = True
+#                     if len(frame.features) != 0:
+#                         self.video.add(frames)
+
+ #####  todo #####
     def complete(self):
 #         self.video.fps = self.jansen.screen.fps   ## I'm not sure how to handle this line yet
         if not self.realTime:    
@@ -71,50 +113,46 @@ class Vision(QTask):
                                         search_range=self.linkTol,
                                         memory=int(self.skip+3))
         self.sigNewVisionVideo(self.video)
+#     @pyqtSlot()
+#     def cleanup(self):
+#         if self.saveFrames or self.saveTrajectories:
+#             omit, omit_feat = ([], [])
+#             if not self.saveFrames:
+#                 omit.append('frames')
+#             if not self.saveTrajectories:
+#                 omit.append('trajectories')
+#             if not self.saveFeatureData:
+#                 omit_feat.append('data')
+#             filename = self.jansen.dvr.filename.split(".")[0] + '.json'
+#             out = self.video.serialize(omit=omit,
+#                                        omit_frame=['data'],
+#                                        omit_feat=omit_feat)
+#             self._writer = QWriter(out, filename)
+#             self.jansen.screen.sigNewFrame.connect(self._writer.write)
+#             self._thread = QThread()
+#             self._writer.moveToThread(self._thread)
+#             self._thread.started.connect(self._writer.start)
+#             self._writer.finished.connect(self.close)
+#             self._thread.start()
+#         self.video = Video(instrument=self.instrument)
+
+#     @pyqtSlot()
+#     def close(self):
+#         logger.debug('Shutting down save thread')
+#         self._thread.quit()
+#         self._thread.wait()
+#         self._thread = None
+#         self._video = None
+#         logger.debug('Save thread closed')     
+
+ #################
 
 
     
-
-    #
-    # Methods
-    #
-    def predict(self, images, framenumbers, post=False):   #### This is a placeholder method for CNNLorenzMie framework
-        frames = []
-        detections = []
-        for image in images:
-            frames.append(Frame())
-            detections.append([])
-        return frames, detections
-
-
-    def getRgb(self, cmap, gray):
-        clr = cmap(gray)
-        rgb = []
-        for i in range(len(clr)):
-            rgb.append(int(255*clr[i]))
-        return tuple(rgb)
-
-    #
-    # PyQt serialized properties
-    #
-    @pyqtProperty(bool)
-    def realTime(self):
-        return self._realTime
-
-    @realTime.setter
-    def realTime(self, realTime):
-        self._postProcess = not realTime
-        self._realTime = realTime
-
-#     @pyqtProperty(bool)
-#     def postProcess(self):
-#         return self._postProcess
-
-#     @postProcess.setter
-#     def postProcess(self, postProcess):
-#         self._postProcess = postProcess
-#         self._realTime = not postProcess
-
+    def predict(self, frames, post=False):
+        finished = self.post if post else self.realtime
+        if self.setting is settings.raw:
+           
 
     @pyqtProperty(float)
     def confidence(self):
