e80d3c6892dae421f7dd5271aa79c6974cd256aa
David Grier <david.grier@nyu.edu>
2020-07-13 15:36:33 -0400

    flip flag when done

--- pyfablib/traps/QTrappingPattern.py
+++ pyfablib/traps/QTrappingPattern.py
@@ -77,6 +77,7 @@ class QTrappingPattern(pg.ScatterPlotItem):
         if self._hologramOutdated:
             traps = self.traps.flatten()
             self.sigCompute.emit(traps)
+            self._hologramOutdated = False
             logger.debug('refreshHologram')
 
     def selectedPoint(self, position):
