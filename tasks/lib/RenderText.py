4592bc6184cab755958b9b536e4086f2eb7419e1
Jackie Sustiel <43007813+sustielz@users.noreply.github.com>
2020-07-21 10:09:33 -0400

    Revert "use pyqtProperty"
    This reverts commit 2526076e3953c5c593a0a7ec778a25a3e39d0a9a.

--- tasks/lib/RenderText.py
+++ tasks/lib/RenderText.py
@@ -36,12 +36,10 @@ class RenderText(QTask):
         bmp = bmp[::-1]
         sz = self.parent().screen.camera.size()
         y, x = np.nonzero(bmp)
-        x += normal(scale=self.fuzz, size=len(x)) - np.mean(x)
-        y += normal(scale=self.fuzz, size=len(y)) - np.mean(y)
-        x *= self.spacing
-        y *= self.spacing
-        x += sz.width() / 2 + self.parent().camera.device.width/2
-        y += sz.height() / 2 + self.parent().camera.device.height/2
+        x = x + normal(scale=self.fuzz, size=len(x)) - np.mean(x)
+        y = y + normal(scale=self.fuzz, size=len(y)) - np.mean(y)
+        x = x * self.spacing + sz.width() / 2
+        y = y * self.spacing + sz.height() / 2
         p = list(map(lambda x, y: QVector3D(x, y, 0), x, y))
         return p
 
