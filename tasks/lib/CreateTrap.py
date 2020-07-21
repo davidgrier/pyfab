7f02d38e42b4c85fd8014f6894c52c935b43e8e6
Jackie Sustiel <43007813+sustielz@users.noreply.github.com>
2020-07-21 10:09:33 -0400

    Revert "More descriptive name"
    This reverts commit 26892779c66dbf35b6b0db3405b17e7bef09e04e.

--- tasks/lib/CreateTrap.py
+++ tasks/lib/CreateTrap.py
@@ -5,11 +5,11 @@ from ..QTask import QTask
 from PyQt5.QtGui import QVector3D
 
 
-class OpticalTweezer(QTask):
+class CreateTrap(QTask):
     '''Add an optical tweezer to the trapping pattern'''
 
     def __init__(self, **kwargs):
-        super(OpticalTweezer, self).__init__(**kwargs)
+        super(CreateTrap, self).__init__(**kwargs)
 
     def complete(self):
         cgh = self.parent().cgh.device
