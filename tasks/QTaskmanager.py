5437d50994f03d62c0ae2aa0d685a033a0b377d8
Jackie Sustiel <43007813+sustielz@users.noreply.github.com>
2020-07-21 10:09:33 -0400

    Revert "simplify dequeuing tasks"
    This reverts commit 78dc6c63375b51bf69ca889977db9df031f1fefd.

--- tasks/QTaskmanager.py
+++ tasks/QTaskmanager.py
@@ -21,11 +21,6 @@ class QTaskmanager(QObject):
     Video frames are passed to the active task by handleTask().
     Once the active task is complete, it is cleaned up and replaced
     with the next task from the queue.
-
-    Non-blocking tasks are registered by setting blocking=False
-    in the call to registerTask(). Such background tasks
-    start running when registered and run in parallel with the
-    task queue without blocking queued tasks.
     """
 
     sigPause = pyqtSignal(bool)
@@ -49,12 +44,15 @@ class QTaskmanager(QObject):
                                  blocking=blocking, **kwargs)
             except ImportError as err:
                 logger.error('Could not import {}: {}'.format(task, err))
-                task = None
+                return
         self.queueTask(task)
         return task
 
     def connectSignals(self, task):
-        task.sigDone.connect(self.dequeueTask)
+        if task.blocking:
+            task.sigDone.connect(self.dequeueTask)
+        else:
+            task.sigDone.connect(lambda task: self.delistTask(task))
         self.sigPause.connect(task.pause)
         self.sigStop.connect(task.stop)
         self.source.sigNewFrame.connect(task.handleTask)
@@ -84,14 +82,16 @@ class QTaskmanager(QObject):
 
     @pyqtSlot()
     def dequeueTask(self):
-        """Removes completed task from task queue or background list"""
-        task = self.sender()  # reference to completed task
+        """Removes task from task queue"""
+        self.disconnectSignals(self.task)
+        self.task = None
+        self.queueTask()
+
+    @pyqtSlot(QTask)
+    def delistTask(self, task):
+        """Removes task from list of background tasks"""
         self.disconnectSignals(task)
-        if task.blocking:
-            self.task = None
-            self.queueTask()
-        else:
-            self.bgtasks.remove(task)
+        self.bgtasks.remove(task)
 
     @pyqtProperty(bool)
     def paused(self):
