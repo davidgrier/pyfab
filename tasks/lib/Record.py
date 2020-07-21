999f5e8bae0244832116164a53da242ef66ed69e
Jackie Sustiel <43007813+sustielz@users.noreply.github.com>
2020-07-21 10:09:33 -0400

    Revert "remove extraneous condition"
    This reverts commit 067a5345080d9d1693842c1622fa9b361974868a.

--- tasks/lib/Record.py
+++ tasks/lib/Record.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
-# MENU: DVR/Record
 
-'''Task to record a specified number of frames.'''
+'''Task to record a video for a certain number of frames.'''
 
 from ..QTask import QTask
 
@@ -9,6 +8,7 @@ from ..QTask import QTask
 class Record(QTask):
 
     def __init__(self, fn=None, nframes=30, **kwargs):
+        # Pass in nframes keyword for length of recording
         super(Record, self).__init__(nframes=nframes, **kwargs)
         self.fn = fn
 
@@ -19,4 +19,5 @@ class Record(QTask):
         self.dvr.recordButton.animateClick()
 
     def complete(self):
-        self.dvr.stopButton.animateClick()
+        if self.stop:
+            self.dvr.stopButton.animateClick()
