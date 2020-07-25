f122150ec48a4e4f217a2a58fea5a8e2c8b2a7db
Jackie Sustiel <43007813+sustielz@users.noreply.github.com>
2020-07-24 21:28:42 -0400

    Create Encircle.py
    An example subclass for MoveTraps. To move along predefined trajectories, pass it in as a keyword trajectories; or to compute trajectories as a function of parameters and trap positions, subclass and override parametrize() as shown.

---
+++ tasks/lib/Encircle.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+# MENU: Motion/Encircle
+
+from .MoveTraps import MoveTraps
+import numpy as np
+
+
+class Encircle(MoveTraps):
+    """Make particles move in a circle around some point"""
+    
+    def __init__(self, **kwargs):
+        super(Encircle, self).__init__(smooth=True, **kwargs)
+        self.nframes = self.nframes or 60
+        self.center = (self.parent().cgh.device.xc, self.parent().cgh.device.yc)
+        print('center is at {}'.format(self.center))
+    
+    def parameterize(self, traps):
+        print('FRAME {}: parametertizing {} traps'.format(self._frame, len(traps)))
+        trajs = {}
+        theta = np.linspace(0, 2*np.pi, self.nframes)
+        xc, yc = self.center
+        for trap in traps:
+            (xi, yi, z) = (trap.x, trap.y, trap.z)
+            print('trap is at {}'.format((xi, yi)))
+            dx = xi - xc
+            dy = yi - yc
+            r = np.sqrt(np.sum(dx**2 + dy**2))
+            phi = np.arctan2(dy, dx)
+            print('r, theta is {}, {}*2pi'.format(r, phi/(2*np.pi)))
+            x = xc + r*np.cos(theta + phi)
+            y = yc + r*np.sin(theta + phi)
+            trajs[trap] = [(x[i], y[i], z) for i in range(self.nframes)]
+        self.trajectories = trajs
+        
