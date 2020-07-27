83d3c53a828421d5e53766102c69eb8a005f28a9
Jackie Sustiel <43007813+sustielz@users.noreply.github.com>
2020-07-27 14:23:57 -0400

    Create RecordBackground.py
    Task to move traps off-screen, acquire a background image, and return traps to original position

---
+++ tasks/lib/RecordBackground.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+# MENU: Experiments/Record Background
+
+from ..QTask import QTask
+import numpy as np
+import json
+
+
+class RecordBackground(QTask):
+    """Make particles move in a circle around some point"""
+    
+    def __init__(self, stepSize=1., **kwargs):
+        self.stepSize = stepSize
+        super(RecordBackground, self).__init__(**kwargs)
+        
+    def initialize(self, frame):
+        cgh = self.parent().cgh.device
+        mpp = cgh.cameraPitch/cgh.magnification  # [microns/pixel]
+        stepSize = self.stepSize / mpp
+        
+        traps = self.parent().pattern.traps.flatten()
+        ext = max( [max(trap.x, trap.y) for trap in traps ] ) + 5*stepSize   # Leave room at the edge so particle is actually off screen
+        dri = list(np.arange(0, ext, stepSize))    
+        trajectories = [ [(trap.x-dr, trap.y-dr, trap.z) for dr in dri] for trap in traps]
+                
+        self.register('MoveTraps', smooth=False, trajectories=trajectories)
+        self.register('Record', nframes=10, fn='background.avi')
+        for traj in trajectories: traj.reverse()
+        self.register('MoveTraps', smooth=False, trajectories=trajectories, delay=10)
