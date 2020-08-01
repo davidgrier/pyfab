bac754b020dfc6a894f5741c00416919482edc86
David Grier <david.grier@nyu.edu>
2020-08-01 08:35:04 -0400

    explain how text rendering works

--- tasks/lib/RenderText.py
+++ tasks/lib/RenderText.py
@@ -24,30 +24,39 @@ class RenderText(QTask):
         '''Returns coordinates of lit pixels in text'''
         if len(self.text) == 0:
             return []
+        # Buffer for drawing text
         w, h = 100, 20
         pixmap = QPixmap(w, h)
         pixmap.fill(QColor('black'))
+
+        # Painter for rendering text
         painter = QPainter(pixmap)
 
+        # White text on black background
         pen = QPen()
         pen.setWidth(1)
         pen.setColor(QColor('white'))
         painter.setPen(pen)
 
+        # Small sans serif font, no antialiasing
         font = QFont()
         font.setFamily('Arial')
         font.setPointSize(9)
         font.setStyleStrategy(QFont.NoAntialias)
         painter.setFont(font)
 
+        # Draw text into buffer
         painter.drawText(0, h, self.text)
         painter.end()
 
+        # Extract bitmap data from buffer
         image = pixmap.toImage()
         data = image.bits()
         data.setsize(h*w*4)
         bmp = np.frombuffer(data, np.uint8).reshape((h, w, 4))
         bmp = bmp[:,:,0]
+
+        # Coordinates of lit pixels
         y, x = np.nonzero(bmp)
         y *= -1
         x = x + normal(scale=self.fuzz, size=len(x)) - np.mean(x)
