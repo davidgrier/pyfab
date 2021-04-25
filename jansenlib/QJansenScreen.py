eb962285a17b05439bc2d93454dc507a1e680edd
David Grier <david.grier@nyu.edu>
2021-04-25 16:18:55 -0400

    printing is not thread safe

--- jansenlib/QJansenScreen.py
+++ jansenlib/QJansenScreen.py
@@ -11,7 +11,7 @@ import time
 import logging
 logging.basicConfig()
 logger = logging.getLogger(__name__)
-logger.setLevel(logging.INFO)
+logger.setLevel(logging.ERROR)
 
 
 class QCameraThread(QThread):
